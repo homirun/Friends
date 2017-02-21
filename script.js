a8a844ff2224ad3bf0695283b2262941215ec4bd
homirun <homirun@homirunnoMacBook-Pro.local>
2017-02-21 23:35:02 +0900

    コードを書くのが苦手なフレンズなんだね！へーきへーき！フレンズによってとくいなことちがうから！

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(function(){
 	$("body").html(
 		$("body").html().replace(/人/g, "フレンズ")
-		$("body").html().replace(/people/,"friends")	);
+	);
+	$("body").html(
+		$("body").html().replace(/people/g,"friends")
+	);
 });
