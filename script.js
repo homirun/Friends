2d5874c2b7cb6119eace8a59512c0b1daf16b3cc
homirun <homirun@homiruns-MacBook-Pro.local>
2017-05-10 11:36:21 +0900

    ちょっと変換できる言葉が増えたよすごーい

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ $(function(){
 	$("body").html(
 		$("body").html().replace(/people/g,"friends")
 	);
+	$("body").html(
+		$("body").html().replace(/死ね/g,"すごーい！")
+	);
+	$("body").html(
+		$("body").html().replace(/のひと/g,"なフレンズ")
+	);
 });
